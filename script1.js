4005621e4464726dd6d8807f986409504741f136
Ankit Kumar Jha <48297878+Mental-bicep@users.noreply.github.com>
2022-04-06 21:09:47 +0530

    improved UI and added some functionality

--- script1.js
+++ script1.js
@@ -1,12 +1,43 @@
 let video=document.querySelector("video");
+let recordBox=document.querySelector(".record-box");
+let recordBtn=recordBox.querySelector(".record-btn");
+let captureBox=document.querySelector(".capture-box");
+let captureBtn=captureBox.querySelector(".capture-btn");
+
+let captureFlag=false;
+let recordFlag=false;
+let mediaRecorder;
 
 let constraints={
     audio:true,
     video:true
 }
-console.log("working fine");
+// console.log("working fine");
 // navigator -> global window object
 navigator.mediaDevices.getUserMedia(constraints).then((stream)=>{
     video.srcObject=stream;
+    console.log(stream);
+    mediaRecorder=new MediaRecorder(stream);
+    console.log(mediaRecorder);
 })
 
+recordBox.addEventListener("click",(e)=>{
+    if(!mediaRecorder) return ;
+    if(recordFlag==false){ 
+        recordBtn.classList.add("scale-record");
+        console.log("started");
+        mediaRecorder.start();
+    }
+    else {
+        mediaRecorder.stop();
+        recordBtn.classList.remove("scale-record");
+    }
+    recordFlag=!recordFlag;
+})
+
+captureBox.addEventListener("click",(e)=>{
+    if(captureFlag==false) captureBtn.classList.add("scale-capture");
+    else captureBtn.classList.remove("scale-capture");
+
+    captureFlag=!recordFlag;
+})
